a1df7702605bb056328c1b018500ed2dd50a3020
Armando Rodríguez Vera <jarodriguezvera@gmail.com>
2017-12-20 13:16:16 -0600

    Se ajustó el margen de los textos, así como del resto de los elementos.

---
+++ app/src/main/java/mx/unam/unica/curso/formulacalendario/ConfirmaDatos.java
@@ -0,0 +1,14 @@
+package mx.unam.unica.curso.formulacalendario;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConfirmaDatos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirma_datos);
+    }
+
+}
