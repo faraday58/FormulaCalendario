e5f9fb5b148e237a73d3853ff2c4cf449eeae627
Armando Rodríguez Vera <jarodriguezvera@gmail.com>
2017-12-22 00:17:48 -0600

    Se ajustó el margen de los textos, así como del resto de los elementos. Nuevos cambios

--- app/src/main/java/mx/unam/unica/curso/formulacalendario/ConfirmaDatos.java
+++ app/src/main/java/mx/unam/unica/curso/formulacalendario/ConfirmaDatos.java
@@ -1,14 +1,55 @@
 package mx.unam.unica.curso.formulacalendario;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ConfirmaDatos extends AppCompatActivity {
+
+   TextView textv_nombre;
+   TextView textv_fecha;
+   TextView textv_telefono;
+   TextView textv_email;
+   TextView textv_descripcion;
+   Button btn_editar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirma_datos);
+        Bundle variables = getIntent().getExtras();
+        String nombre = variables.getString(getResources().getString(R.string.extra_nombre));
+        String telefono = variables.getString(getResources().getString(R.string.extra_telefono));
+        String email = variables.getString(getResources().getString(R.string.extra_email));
+        String fecha = variables.getString(getResources().getString(R.string.extra_fecha));
+        String descripcion = variables.getString(getResources().getString(R.string.extra_descripcion));
+
+        textv_nombre = (TextView)findViewById(R.id.textv_nombre);
+        textv_telefono =(TextView)findViewById(R.id.textv_telefono);
+        textv_email =(TextView)findViewById(R.id.textv_email);
+        textv_fecha = (TextView)findViewById(R.id.textv_fecha);
+        textv_descripcion = (TextView)findViewById(R.id.textv_descripcion);
+        btn_editar = (Button)findViewById(R.id.btn_editar);
+
+        textv_nombre.setText(textv_nombre.getText().toString() + " " + nombre);
+        textv_telefono.setText(textv_telefono.getText().toString() + " " + telefono);
+        textv_email.setText(textv_email.getText().toString() + " " + email);
+        textv_fecha.setText(textv_fecha.getText().toString() + " " + fecha);
+        textv_descripcion.setText(textv_descripcion.getText().toString() + " " + descripcion);
+
+
+        btn_editar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent miIntent = new Intent(view.getContext(),MainActivity.class);
+                startActivity(miIntent);
+                finish();
+            }
+        });
+
     }
 
 }
--- app/src/main/java/mx/unam/unica/curso/formulacalendario/MainActivity.java
+++ app/src/main/java/mx/unam/unica/curso/formulacalendario/MainActivity.java
@@ -1,13 +1,86 @@
 package mx.unam.unica.curso.formulacalendario;
 
+import android.content.Intent;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_ok;
+    Button btn_cancel;
+    Button btn_siguiente;
+    DatePicker dtp_fnacimiento;
+    TextInputEditText texti_nombre;
+    TextInputEditText texti_telefono;
+    TextInputEditText texti_email;
+    TextInputEditText texti_descripcion;
+    int year;
+    int month;
+    int day;
+    String fecha;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_ok = (Button)findViewById(R.id.btn_ok);
+        btn_cancel = (Button)findViewById(R.id.btn_cancel);
+        dtp_fnacimiento = (DatePicker)findViewById(R.id.dtp_fnacimiento);
+        btn_siguiente = (Button)findViewById(R.id.btn_siguiente);
+        texti_nombre = (TextInputEditText)findViewById(R.id.texti_nombre);
+        texti_telefono = (TextInputEditText)findViewById(R.id.texti_telefono);
+        texti_email =(TextInputEditText)findViewById(R.id.texti_email);
+        texti_descripcion =(TextInputEditText)findViewById(R.id.texti_descripcion);
+
+
+        btn_ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               year = dtp_fnacimiento.getYear();
+               month = dtp_fnacimiento.getMonth();
+               day = dtp_fnacimiento.getDayOfMonth();
+               fecha = day +"/" + month + "/" + year;
+                Snackbar.make(view,fecha,Snackbar.LENGTH_LONG).show();
+
+            }
+        });
+
+        btn_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View view) {
+                dtp_fnacimiento.init(1985, 10, 02, new DatePicker.OnDateChangedListener() {
+                    @Override
+                    public void onDateChanged(DatePicker datePicker, int i, int i1, int i2) {
+                        Snackbar.make(view,"Reestableciendo fecha"+i +"/" + i1 + "/" + i2,Snackbar.LENGTH_LONG).show();
+
+                    }
+                });
+            }
+        });
+
+        btn_siguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentDatos = new Intent(view.getContext(),ConfirmaDatos.class);
+                intentDatos.putExtra(getResources().getString(R.string.extra_nombre),texti_nombre.getText().toString());
+                intentDatos.putExtra(getResources().getString(R.string.extra_telefono),texti_telefono.getText().toString());
+                intentDatos.putExtra(getResources().getString(R.string.extra_email),texti_email.getText().toString());
+                intentDatos.putExtra(getResources().getString(R.string.extra_descripcion),texti_descripcion.getText().toString());
+                intentDatos.putExtra(getResources().getString(R.string.extra_fecha),fecha);
+                startActivity(intentDatos);
+            }
+        });
+
+
     }
+
+
+
 }
